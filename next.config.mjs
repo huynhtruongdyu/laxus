0d1f1de2d223cdb7c40f715da9ac587c331ed6df
Aaron <huynhtruongdyu@gmail.com>
2024-04-15 23:54:21 +0700

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+console.log("__dirname", __dirname);
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
 
 export default nextConfig;
