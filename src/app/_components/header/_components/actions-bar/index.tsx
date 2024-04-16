613ba050352297c4f1c42932986e3d1b29925f1d
Duy Huynh <huynhtruongdyu@gmail.com>
2024-04-16 17:42:25 +0700

    feat(header): add fullscreen toggle button

--- src/app/_components/header/_components/actions-bar/index.tsx
+++ src/app/_components/header/_components/actions-bar/index.tsx
@@ -6,10 +6,34 @@ import {
   BiSolidMessageSquareDetail,
   BiSolidNotification,
 } from "react-icons/bi";
+import { AiOutlineFullscreen, AiOutlineFullscreenExit } from "react-icons/ai";
+import { MdFullscreen, MdFullscreenExit } from "react-icons/md";
 
 import "./styles.css";
+import { useCallback, useEffect, useState } from "react";
 
 const ActionBar = () => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  const toggleFullScreen = useCallback(() => {
+    if (!isFullScreen) {
+      document.documentElement.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+    setIsFullScreen(!isFullScreen);
+  }, [isFullScreen]);
+
+  useEffect(() => {
+    const handleFullScreenChange = () => {
+      setIsFullScreen(document.fullscreenElement !== null);
+    };
+
+    document.addEventListener("fullscreenchange", handleFullScreenChange);
+
+    return () =>
+      document.removeEventListener("fullscreenchange", handleFullScreenChange);
+  }, []);
   return (
     <div className="header__actionbar">
       <a className="header__actionbar__item" role="button" title="Message">
@@ -36,6 +60,17 @@ const ActionBar = () => {
       >
         {/* <BiNotification size={"1.5rem"} /> */}
         <BiSolidNotification size={"1.5rem"} />
+      </a>
+      <a
+        className="header__actionbar__item active"
+        role="button"
+        onClick={toggleFullScreen}
+      >
+        {isFullScreen ? (
+          <MdFullscreenExit size={"1.6rem"} />
+        ) : (
+          <MdFullscreen size={"1.6rem"} />
+        )}
       </a>
     </div>
   );
