8f00166b9aea8443b332b6256d343d0eb143e21c
Duy Huynh <huynhtruongdyu@gmail.com>
2024-04-18 01:19:54 +0700

    feat(about): init hero section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import "./styles.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="about-container">
+      <div className="about-hero">
+        <div className="about-hero__background"></div>
+        <div className="about-hero__content glass">
+          <h1 title={`Console.WriteLine("hello world")`}>
+            i'm a <span>backend developer</span>
+          </h1>
+        </div>
+      </div>
+      <div className="about-content">
+        {/* Multiple Typing Text Animation */}
+        <section className="about-section about-section__info">
+          Common Info
+        </section>
+        <section className="about-section about-section__timeline">
+          Timeline
+        </section>
+      </div>
     </div>
   );
 };
